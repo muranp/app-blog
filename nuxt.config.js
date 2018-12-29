1d90631dd164b3e5b0a0e658c8965293c4426707
YukiOta <yuki.ota@ruri.waseda.jp>
2018-12-29 16:44:17 +0900

    :zap: test

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,12 +51,15 @@ module.exports = {
   /*
    ** env setting
    */
-  env: envSetPub,
+  env: {
+    ...envSetPub,
+    CTF_SPACE_ID: process.env.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN
+  },
   /*
    ** plugins
    */
-  plugins: [
-  ],
+  plugins: [],
   /*
    ** modules
    */
